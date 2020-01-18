a8132fa81238d7747a38e981ba47593a1339207c
silan <siland.dope@yahoo.fr>
2020-01-18 14:34:01 -0800

    fixet bug

--- server.js
+++ server.js
@@ -1,77 +1,73 @@
 'use strict';
-const superagent = require('superagent');
-const express = require('express');
-const app = express();
-require('dotenv').config();
 
+const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
-let locations = {};
+const superagent = require('superagent');
+const pg =require('pg');
+const app = express();
 
 // database connection set up
-const pg =require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
-client.on ('error', err => {throw err;});
+client.on ('error', err => console.error(err));
 
-//  Declare variable
-app.use(cors());
+let locations = {};
+
+
 const PORT = process.env.PORT || 3001;
 
 // use corss to allow to past data to front end
 
 app.use(cors());
 app.get('/location', locationHandler);
+// app.getMaxListeners('/events', enventfulHandler);
 
 
 // define routes
 function locationHandler(request, response) {
 
-  try {
-    // const dataArray = require('./data/geo.json');
-    let city = request.query.city;
-    let key = process.env.GEOCODE_API_KEY;
-    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
-    let SQL = 'select * from locations where search_query = $1;';
-    let values = [city];
 
-  
+  // const dataArray = require('./data/geo.json');
+  let city = request.query.city;
+  let key = process.env.GEOCODE_API_KEY;
 
-    return client.query(SQL, values)
-      .then(result => {
-        // console.log(result);
+  let SQL = 'select * from locations where search_query = $1;';
+  let values = [city];
 
-        // if there is a city in DB
-        // results.rows = ['seattle', '55585']
+  client.query(SQL, values)
+    .then(result => {
 
-        // if theres no city in the DB
-        // results.rowss = [];
-        if (result.rowCount) {
-          console.log(result.rows);
-          response.send(result.rows[0]);
-        }
-        // hit this condition when there isn't city searched in database
-        else {
-          superagent.get(url)
-            .then(data => {
-              // add to DB
-              const geoData = data.body[0];
-              const location = new Location(city, geoData);
-              locations[url] = location;
-              response.send(location);
-            })
-            .catch(() => {
-              errorHandler('If you did not get result. Please, try again', request, response);
-            });
+      console.log(result.rows);
+
+      if(result.rows.length > 0){
+        console.log('result foud in data base');
+        response.send(result.rows[0]);
+      }
+
+      // hit this condition when there isn't city searched in database
+      else {
+        console.log('did not find result going to superagent api');
+        const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+
+        superagent.get(url)
+          .then(data => {
+            // add to DB
+            // const geoData = data.body[0];
+            const location = new Location(city, data.body[0]);
+            // locations[url] = location;
+            let sql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
+            let safeValues = [location.search_query, location.formatted_query, location.latitude, location.longitude];
+            client.query(sql, safeValues);
+            response.status(200).send(location);
+          })
+          .catch(() => {
+            errorHandler('If you did not get result. Please, try again', request, response);
+          });
+      }
+
+    });
 
 
-        }
-
-      });
-
-
-  }
-  catch(error) {
-    errorHandler('If you did not get results. Please try again.', response);
-  }
 }
 
 
@@ -101,7 +97,28 @@ app.get('/weather', (request, response) => {
 });
 
 
+app.get('/events', (request, response) => {
+  let key = process.env.EVENTFUL_API_KEY;
+  let {search_query }= request.query;
+  const eventDataUrl =`http://api.eventful.com/json/events/search?keywords=music&location=${search_query}&app_key=${process.env.EVENTFUL_API_KEY}`;
+
+  superagent.get(eventDataUrl)
+    .then(eventData => {
+      let eventMassData = JSON.parse(eventData.text);
+      let localEvent =eventMassData.events.event.map(thisEventData => {
+        return new NewEvent(thisEventData);
+      });
+      response.status(200).send(localEvent);
+    })
+    .catch(() => {
+      errorHandler('If you did not get result. Please, try again', request, response);
+    });
+
+});
+
+
 // Define function
+
 
 
 
@@ -123,16 +140,22 @@ function errorHandler(string, response) {
   response.status(500).send(string);
 }
 
+function NewEvent(thisEventData) {
+  this.name = thisEventData.title;
+  this.event_date = thisEventData.start_time.slice(0,10);
+  this.link = thisEventData.url;
+  this.summary = thisEventData.description;
+
+}
+
 // Make sure the server is listening for requests
 // app.listen(PORT, () => console.log(`Never Give up ${PORT}`));
 
 // connect to BD and Start the web server
 client.connect()
-  .then(() => {
+  .then(
     app.listen(PORT, () => {
       console.log('Never Give up', PORT);
-    });
-  })
-  .catch(err => {
-    throw `PG Startup Error: ${err.message}`;
-  });
+    })
+  )
+  .catch(err => console.error(err));
